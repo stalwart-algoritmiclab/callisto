6d9160c67cc52d6baac4c592d69d3610bb72ec07
Vasyl Naumenko <vnau108@gmail.com>
2024-05-24 17:46:13 +0300

    imported protos

---
+++ proto/stwartchain/faucet/msg_update_params.go
@@ -0,0 +1,25 @@
+package faucet
+
+import (
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+var _ sdk.Msg = &MsgUpdateParams{}
+
+// ValidateBasic does a sanity check on the provided data.
+func (m *MsgUpdateParams) ValidateBasic() error {
+	if _, err := sdk.AccAddressFromBech32(m.Authority); err != nil {
+		return errorsmod.Wrap(err, "invalid authority address")
+	}
+
+	if err := m.Params.Validate(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (msg *MsgUpdateParams) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{}
+}
