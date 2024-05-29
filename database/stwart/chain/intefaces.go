575563672b4e7270b891619facd112f09dca4e59
Vasyl Naumenko <vnau108@gmail.com>
2024-05-29 16:54:46 +0300

    rates Crud update (#14)
    * rates Crud update
    
    * rename migration
    
    * add addresses crud

--- database/stwart/chain/intefaces.go
+++ database/stwart/chain/intefaces.go
@@ -53,8 +53,21 @@ type (
 	Rates interface {
 		GetAllMsgCreateAddresses(filter filter.Filter) ([]rates.MsgCreateAddresses, error)
 		InsertMsgCreateAddresses(hash string, msgs ...*rates.MsgCreateAddresses) error
+
+		GetAllMsgUpdateAddresses(filter filter.Filter) ([]rates.MsgUpdateAddresses, error)
+		InsertMsgUpdateAddresses(hash string, msgs ...*rates.MsgUpdateAddresses) error
+
+		GetAllMsgDeleteAddresses(filter filter.Filter) ([]rates.MsgDeleteAddresses, error)
+		InsertMsgDeleteAddresses(hash string, msgs ...*rates.MsgDeleteAddresses) error
+
 		GetAllMsgCreateRates(filter filter.Filter) ([]rates.MsgCreateRates, error)
 		InsertMsgCreateRates(hash string, msgs ...*rates.MsgCreateRates) error
+
+		GetAllMsgUpdateRates(filter filter.Filter) ([]rates.MsgUpdateRates, error)
+		InsertMsgUpdateRates(hash string, msgs ...*rates.MsgUpdateRates) error
+
+		GetAllMsgDeleteRates(filter filter.Filter) ([]rates.MsgDeleteRates, error)
+		InsertMsgDeleteRates(hash string, msgs ...*rates.MsgDeleteRates) error
 	}
 
 	// Secured - describes an interface for working with secured database models.
---
+++ modules/stwart/chain/rates/handle_msg_addresses.go
@@ -0,0 +1,32 @@
+package rates
+
+import (
+	juno "github.com/forbole/juno/v5/types"
+
+	"github.com/stalwart-algoritmiclab/callisto/proto/stwartchain/rates"
+)
+
+// handleMsgCreateAddresses allows to properly handle a MsgCreateAddresses
+func (m *Module) handleMsgCreateAddresses(tx *juno.Tx, msg *rates.MsgCreateAddresses) error {
+	return m.ratesRepo.InsertMsgCreateAddresses(tx.TxHash, &rates.MsgCreateAddresses{
+		Creator: msg.Creator,
+		Address: msg.Address,
+	})
+}
+
+// handleMsgUpdateAddresses allows to properly handle a MsgUpdateAddresses
+func (m *Module) handleMsgUpdateAddresses(tx *juno.Tx, msg *rates.MsgUpdateAddresses) error {
+	return m.ratesRepo.InsertMsgUpdateAddresses(tx.TxHash, &rates.MsgUpdateAddresses{
+		Creator: msg.Creator,
+		Id:      msg.Id,
+		Address: msg.Address,
+	})
+}
+
+// handleMsgDeleteAddresses allows to properly handle a MsgDeleteAddresses
+func (m *Module) handleMsgDeleteAddresses(tx *juno.Tx, msg *rates.MsgDeleteAddresses) error {
+	return m.ratesRepo.InsertMsgDeleteAddresses(tx.TxHash, &rates.MsgDeleteAddresses{
+		Creator: msg.Creator,
+		Id:      msg.Id,
+	})
+}
