dad61b605a027d46fe159d9994e1634c9854e536
“achuyko” <achuyko@algoritmiclab.ai>
2024-06-25 16:44:38 +0300

    Update cosmos version 0.50.x

--- database/schema/09-modules.sql
+++
@@ -1,8 +0,0 @@
--- +migrate Up
-CREATE TABLE modules
-(
-    module_name TEXT NOT NULL UNIQUE PRIMARY KEY
-);
-
--- +migrate Down
-DROP TABLE IF EXISTS modules CASCADE;
--- database/schema/13-last_block.sql
+++
@@ -1,9 +0,0 @@
--- +migrate Up
-CREATE TABLE IF NOT EXISTS last_block
-(
-    block BIGINT NOT NULL
-);
-
-
--- +migrate Down
-DROP TABLE IF EXISTS last_block;
